a0fd76183c83abbc95c536cf836e2437e0722fd4
bibek <bibekdahal.bd16@gmail.com>
2016-08-07 10:12:33 +0545

    Fix home url in server

--- togglecorp/urls.py
+++ togglecorp/urls.py
@@ -21,6 +21,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
 
     url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'^home/$', TemplateView.as_view(template_name="index.html")),
 
     url(r'^admin/', admin.site.urls),
 ]
