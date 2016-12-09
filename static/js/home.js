067fbf606a837b9eab1c3c1c71fc8797a9a47598
Aditya Khatri <adityakhatri47@gmail.com>
2016-12-09 15:17:16 +0545

    Redesign everything

---
+++ static/js/home.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+    var navbarFixed = false;
+    $(document).scroll(function() {
+        var scrollPos = $(this).scrollTop();
+        if(scrollPos > 600) {
+            if (!navbarFixed){
+                $('header').addClass('navbar-scrolled');
+                $('#home').css('margin-top', '0');
+                navbarFixed = true;
+            }
+        } else {
+            if (navbarFixed){
+                navbarFixed = false;
+                $('header').fadeOut(function(){
+                    $('header').removeClass('navbar-scrolled');
+                    $('header').show();
+                    $('#home').css('margin-top', '-56px');
+                });
+            }
+        }
+    });
+
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 500);
+                    return false;
+                }
+            }
+        });
+    });
+
+
+
+    // Close menu when a nav item is selected
+    $('#menu ul a').click(function() {
+        $('#menu ul.open').removeClass('open');
+    });
+});
+
+function toggleMenu() {
+    $('#menu ul').toggleClass('open');
+}
