677c2931cd9c1b5473ca9af241cb4fe2b1cf248a
Frozen Helium <fren.ankit@gmail.com>
2017-06-21 07:47:41 +0545

    Make few improvements

--- static/js/home.js
+++ static/js/home.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
     $('.carousel').each(() => {
         new TerminalCarousel($(this), 64, 8, 3000, 200);
     });
+
+    $('.feature>div:first-child').on('click', function() {
+        $('.feature.active').removeClass('active');
+        $(this).closest('.feature').addClass('active');
+    });
 });
